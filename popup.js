957fc0e12fbbee0c49c64051de4f90658cf88e50
Jan Baykara <jan@baykara.co.uk>
2017-04-22 16:45:46 +0100

    Popup loading animation

--- popup.js
+++ popup.js
@@ -1,8 +1,22 @@
+var response = null;
+
 $(document).ready(function() {
-  $.get("https://who-targets-me.herokuapp.com/analytics/", function(raw_response) {
-    var response = $.parseJSON(raw_response)
-    response.data.map(function(analytic, index) {
-      $("tbody").append("<tr><td>" + analytic.entity + "</td><td>" + analytic.instances + "</td></tr>")
-    })
-  });
+	checkLoading();
+	$.get("https://who-targets-me.herokuapp.com/analytics/", function(raw_response) {
+		response = $.parseJSON(raw_response)
+		response.data.map(function(analytic, index) {
+			$("tbody").append("<tr><td>" + analytic.entity + "</td><td>" + analytic.instances + "</td></tr>");
+		});
+		checkLoading();
+	});
 })
+
+function checkLoading() {
+	if (response == null) {
+		$("#isLoading").show();
+		$("#isFinished").hide();
+	} else {
+		$("#isLoading").hide();
+		$("#isFinished").show();
+	}
+}
