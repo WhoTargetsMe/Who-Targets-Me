5897e09e23d59beeeb4838fdab3b268a23a5d417
Jan Baykara <jan@baykara.co.uk>
2017-04-25 02:33:52 +0100

    Simplified advert recording logic, chrome.storage user ad record + browse session diff archiving

--- facebook.js
+++ facebook.js
@@ -1,103 +1,110 @@
-var advertisers = [];
-var savedSnapshots = [];
-var processingAdvertIDs = []; // Will merge with 'savedSnapshots' in due course.
-var storedAdvertIDs = []; // Retrieve from server, all post IDs (i.e. adverts) snapshotted
+var userStorage = new Session({
+	advertHistory: []
+})
+
+var currentSessionHistory = []; // HTML / image data too large to store in `chrome.storage`
 
 Array.prototype.diff = function(a) { // Polyfill diff function
 	return this.filter(function(i) {return a.indexOf(i) < 0;});
 };
 
 $(document).ready(function() {
-	window.setInterval(function(){
-		var updated_advertisers = []
+	window.setInterval(function() {
+		var updatedSessionHistory = []
+
+		thisBatchN = $("a:contains('Sponsored')").length;
 		$("a:contains('Sponsored')").each(function(index) {
 			var advertiserHTML = $(this).closest('div').prev().find('a:first-of-type');
 			var advertiserName = advertiserHTML.text();
 			var top_level_post_id = /\[top_level_post_id\]=([0-9]+)/.exec(advertiserHTML.attr('href'));
 			var advertiserID = advertiserHTML.attr('data-hovercard-obj-id');
 
-			if(advertiserName && (
-				(top_level_post_id != null && top_level_post_id.constructor === Array)
-				/* try data-dedupekey or id=hyperfeed_story_id_... as alternative unique ID? */
-			)) {
+			// Check that it's an identifiable post
+			if(advertiserName && top_level_post_id != null && top_level_post_id.constructor === Array) {
 				top_level_post_id = top_level_post_id[1];
-				updated_advertisers.push(advertiserName);
 				var adContent = $(this).closest('div').prev().find('a:first-of-type').closest('.fbUserContent');
 
-				// Store new adverts to DB
-				if(!processingAdvertIDs.includes(top_level_post_id) && !storedAdvertIDs.includes(top_level_post_id)) {
-					processingAdvertIDs.push(top_level_post_id);
+				// Get image/video thumbnail URL
+				if(adContent.find('.fbStoryAttachmentImage')) {
+					var thumbnailMedia = adContent.find('.fbStoryAttachmentImage img.scaledImageFitWidth').attr('src');
+				} else if(adContent.find('video')) {
+					var thumbnailMedia = adContent.find('video[muted]').attr('src');
+				} else {
+					thumbnailMedia = null; // fail
+				}
 
-					// Get image/video thumbnail URL
-					if(adContent.find('.fbStoryAttachmentImage')) {
-						var thumbnailMedia = adContent.find('.fbStoryAttachmentImage img.scaledImageFitWidth').attr('src');
-					} else if(adContent.find('video')) {
-						var thumbnailMedia = adContent.find('video[muted]').attr('src');
-					} else {
-						thumbnailMedia = null; // fail
-					}
+				// Get advert link-out
+				var linkTo = adContent.find('.userContent').next().find('a').attr('href');
+				if(linkTo.includes("l.facebook.com/l.php?")) {
+					linkTo = getParameterByName('u',linkTo);
+				} else {
+					linkTo = decodeURIComponent(linkTo);
+				}
 
-					// Get advert link-out
-					var linkTo = adContent.find('.userContent').next().find('a').attr('href');
-					if(linkTo.includes("l.facebook.com/l.php?")) {
-						linkTo = getParameterByName('u',linkTo);
-					} else {
-						linkTo = decodeURIComponent(linkTo);
-					}
+				// Snapshots are sent to storage, and kept in user session history too.
+				var snapshot = {
+					entity: advertiserName,
+					entityID: parseInt(advertiserID),
+					timestamp_created: parseInt(adContent.closest('[data-timestamp]').attr('data-timestamp')),
+					// Divide by 1000 to match the above, which is in seconds, compatibility for PHP
+					timestamp_snapshot: parseInt((Date.now() / 1000).toFixed()),
+					top_level_post_id: parseInt(top_level_post_id),
+					// May need to go through Facebook gateway, to get REAL url?
+					linkTo: linkTo,
+					postText: adContent.find('.userContent').text(),
+					// the big/small text beneath thumbnail images
+					// captures the majority of cases, but not all
+					fbStory_headline: adContent.find('.mbs._6m6._2cnj._5s6c, ._275z._5s6c').text(), // selectors for image, video
+					fbStory_subtitle: adContent.find('._6m7._3bt9, ._5q4r').text(),
+					// Maybe we want to download and save these on our server?
+					// Images are easy to store... but what about fb-locked videos?
+					thumbnailMedia: thumbnailMedia,
+				}
 
-					var snapshot = {
-						entity: advertiserName,
-						entityID: parseInt(advertiserID),
-						timestamp_created: parseInt(adContent.closest('[data-timestamp]').attr('data-timestamp')),
-						// Divide by 1000 to match the above, which is in seconds, compatibility for PHP
-						timestamp_snapshot: parseInt((Date.now() / 1000).toFixed()),
-						top_level_post_id: parseInt(top_level_post_id),
-						html: adContent.html(),
-						// May need to go through Facebook gateway, to get REAL url?
-						linkTo: linkTo,
-						postText: adContent.find('.userContent').text(),
-						// Maybe we want to download and save these on our server?
-						// Images are easy to store... but what about fb-locked videos?
-						thumbnailMedia: thumbnailMedia,
-						// the big/small text beneath thumbnail images
-						// captures the majority of cases, but not all
-						fbStory_headline: adContent.find('.mbs._6m6._2cnj._5s6c, ._275z._5s6c').text(), // selectors for image, video
-						fbStory_subtitle: adContent.find('._6m7._3bt9, ._5q4r').text(),
-					};
+				// snapshot_blobs are for server storage only.
+				var snapshot_blobs = {
+					html: adContent.html()
+				};
 
-					// Image snapshot
+				// Image snapshot
+				if(false) { // Bypass html2canvas for now, it's a tad crap
 					html2canvas(adContent, {
 						onrendered: function(canvas) {
-							snapshot.imageRender = canvas.toDataURL("image/png")
+							snapshot_blobs.imageRender = canvas.toDataURL("image/png")
 							saveSnapshot();
 						}
 					});
-
-					function saveSnapshot() {
-						savedSnapshots.push(snapshot);
-						console.log("[ARCHIVING] Advertiser: "+advertiserName+" - Advert ID: "+top_level_post_id, snapshot);
-
-						// Requires server-side DB compatibility
-						$.post("https://who-targets-me.herokuapp.com/analytics/", snapshot, function( data ) {
-							console.log("[ARCHIVING COMPLETE] Advertiser: "+advertiserName+" - Advert ID: "+top_level_post_id);
-							storedAdvertIDs.push(top_level_post_id); // now update server, too.
-							console.log(storedAdvertIDs);
-						});
-					}
 				} else {
-					// console.log("[ALREADY ARCHIVED] Advertiser: "+advertiserName+" - Advert ID: "+top_level_post_id);
+					saveSnapshot();
+				}
+
+				function saveSnapshot() {
+					// console.log("Noted an ad by "+snapshot.entity,snapshot.top_level_post_id)
+					updatedSessionHistory.push({'snapshot':snapshot,'snapshot_blobs':snapshot_blobs});
+					if(index == thisBatchN-1) {
+						synchronise(updatedSessionHistory);
+					}
 				}
 			}
 		})
-		if(updated_advertisers.length !== advertisers.length) {
-			// saveSponsoringEntity
-			var new_advertisers = updated_advertisers.diff(advertisers)
-			new_advertisers.map(function(advertiser, index) {
-				$.post("https://who-targets-me.herokuapp.com/analytics/", {entity: advertiser}, function( data ) {
-					//console.log(data)
-				});
-			})
-			advertisers = updated_advertisers
+
+		function synchronise(updatedSessionHistory) {
+			console.log("Sync'ing ads just found ("+updatedSessionHistory.length+") w/ ads this session's history ("+currentSessionHistory.length+").");
+			// See if there are adverts new to the `currentSessionHistory`, to be POST'd and SESSION'd
+			if(updatedSessionHistory.length != currentSessionHistory.length) {
+				var newAdverts = updatedSessionHistory.diff(currentSessionHistory)
+				newAdverts.map(function(ad, index) {
+					// Only save small text to user session
+					userStorage.add('advertHistory', ad.snapshot);
+					console.log("New ad [USER SYNC'D] Advertiser: "+ad.snapshot.entity+" - Advert ID: "+ad.snapshot.top_level_post_id, ad.snapshot);
+					// Save the whole shabang to server
+					var wholeShabang = Object.assign({}, ad.snapshot, ad.snapshot_blobs);
+					$.post("https://who-targets-me.herokuapp.com/analytics/", wholeShabang, function( data ) {
+						console.log("This new ad [SERVER SYNC'D] Advertiser: "+wholeShabang.entity+" - Advert ID: "+wholeShabang.top_level_post_id, wholeShabang);
+					});
+				})
+				currentSessionHistory = updatedSessionHistory;
+			}
 		}
 	}, 5000);
 });
