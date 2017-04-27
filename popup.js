317a78b500ea01cb152ac1010b625aaaec079999
Jan Baykara <jan@baykara.co.uk>
2017-04-27 15:40:58 +0100

    Stop old `popup.js` requests to /analytics/

--- popup.js
+++ popup.js
@@ -1,27 +1,6 @@
-var response = null;
-
 $(document).ready(function() {
-	checkLoading();
-	$.get("https://who-targets-me.herokuapp.com/analytics/", function(raw_response) {
-		response = $.parseJSON(raw_response)
-		response.data.map(function(analytic, index) {
-			$("tbody").append("<tr><td>" + analytic.entity + "</td><td>" + analytic.instances + "</td></tr>");
-		});
-		checkLoading();
-	});
-
 	$('body').on('click', 'a', function(){
 		chrome.tabs.create({url: $(this).attr('href')});
 		return false;
 	});
 })
-
-function checkLoading() {
-	if (response == null) {
-		$("#isLoading").show();
-		$("#isFinished").hide();
-	} else {
-		$("#isLoading").hide();
-		$("#isFinished").show();
-	}
-}
