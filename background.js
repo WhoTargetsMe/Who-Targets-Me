8dd82b609820450beb3486b783ae8b0dbda394e8
Jan Baykara <jan@baykara.co.uk>
2017-04-28 00:39:32 +0100

    Use ‘new user’ `config.html` popup if no access_token

--- background.js
+++ background.js
@@ -5,14 +5,22 @@ var userStorage = new ChromeStorage({ // Collect basic targeting data across use
 
 userStorage.onLoad({
 	'access_token': checkForToken
-})
+});
+
+setTimeout(function() {
+	userStorage.onChange({
+		'access_token': checkForToken
+	});
+}, 3000);
 
 function checkForToken() {
 	if(!userStorage.access_token || userStorage.access_token == undefined || userStorage.access_token == nul) {
 		console.log("No valid userStorage.access_token",userStorage.access_token);
+		chrome.browserAction.setPopup({popup: "config.html"});
 		chrome.tabs.create({url: 'config.html' });
 	} else {
 		console.log("User has userStorage.access_token",userStorage.access_token);
+		chrome.browserAction.setPopup({popup: "popup.html"});
 		clearInterval(accessTokenPrompt);
 	}
 }
