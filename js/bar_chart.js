000700fbad1e1c11495211a38766446d8a1abfa1
Richard Vaughan <richard.vaughan@xuvasi.com>
2017-04-29 15:36:15 +0100

    Initial version of the personal demographic breakdown.

---
+++ js/bar_chart.js
@@ -0,0 +1,60 @@
+function render_bar_chart(data) {
+var svg = d3.select("#bar-chart"),
+    margin = {top: 10, right: 10, bottom: 10, left: 120},
+    width = + svg.attr("width") - margin.left - margin.right,
+    height = + svg.attr("height") - margin.top - margin.bottom;
+
+    var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+
+    var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleBand().range([height, 0]);
+
+    var g = svg.append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain([0, d3.max(data,
+                        function(d) {
+                            return d.count;
+                        })
+                ]);
+    y.domain(data.map(function(d) { return d.party; })).padding(0.1);
+
+    g.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x)
+                .ticks(5)
+                .tickFormat(function(d) {
+                        return parseInt(d);
+                    })
+                .tickSizeInner([-height]));
+
+    g.append("g")
+        .attr("class", "y axis")
+        .call(d3.axisLeft(y));
+
+    g.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", function(d, i) {
+                return "bar " + d.party.replace(/\s/g, '').toLowerCase();
+            })
+        .attr("x", 0)
+        .attr("height", y.bandwidth())
+        .attr("y", function(d) {
+                    return y(d.party);
+                })
+        .attr("width", function(d) {
+                        return x(d.count);
+                    })
+        .on("mousemove", function(d){
+            tooltip
+              .style("left", d3.event.pageX - 50 + "px")
+              .style("top", d3.event.pageY - 70 + "px")
+              .style("display", "inline-block")
+              .html((d.party) + "<br>" + (d.count) + " adverts");
+        })
+        .on("mouseout", function(d) {
+                            tooltip.style("display", "none");
+                        });
+}
--- popup.js
+++ popup.js
@@ -16,6 +16,75 @@ chrome.extension.sendMessage({notification: "hide"});
 // Can't access var until it's sync'd
 userStorage.onLoad({ 'access_token': start() })
 
+
+function get_user_analytics_data(failure, success) {
+	data = {
+			demographic: {
+					gender: "woman",
+					age_range: "64-75",
+					constituency: "Clacton"
+				},
+			ads: {
+					breakdown: [
+							{
+								party: "Conservatives",
+								count: 10
+							},
+							{
+								party: "Labour",
+								count: 7
+							},
+							{
+								party: "Liberal Democrats",
+								count: 2
+							},
+							{
+								party: "UKIP",
+								count: 4
+							}
+						],
+					total: 401,
+					ad_cost: 2,
+				}
+		};
+	success(data);
+}
+
+
+function show_user_demographics(data) {
+	$('#demographic').text("As a " + data.gender + ", between " + data.age_range + ", voting in " + data.constituency + ":");
+}
+
+
+function show_user_ad_info(data) {
+	ad_count = 0;
+	$.each(data.breakdown, function (idx, data) {
+			console.log(data);
+			ad_count += data.count;
+		});
+	percent = ((ad_count / data.total) * 100).toFixed(1);
+
+	cost = ((ad_count * data.ad_cost) / 100).toFixed(2);
+
+	$('#ad-percentage').text(percent + "%");
+	$('#ad-cost').html('&pound;' + cost);
+
+	$('ad-summary').text("(Based on seeing " + data.total + " ads, of which " + ad_count + " were political)");
+}
+
+
+function show_user_analytics() {
+	get_user_analytics_data(function(err) {
+			console.log(err);
+		},
+		function(data) {
+			show_user_demographics(data.demographic);
+			show_user_ad_info(data.ads);
+			render_bar_chart(data.ads.breakdown);
+		});
+}
+
+
 function start() {
 	$(document).ready(function() {
 
@@ -36,6 +105,8 @@ function start() {
 			if ((browserStorage.advertArchive) && (browserStorage.advertArchive.length > 10)) {
 				$('#data-not-ready').hide();
 				$('#data-ready').show();
+
+				show_user_analytics();
 			}
 		} else {
 			$("#results").hide();
@@ -59,7 +130,7 @@ function start() {
 function isFormValid() {
 	var request = {};
 	$.each($('#register').serializeArray(), function(i, field) {
-	    request[field.name] = field.value;
+		request[field.name] = field.value;
 		userStorage.set([field.name], field.value.trim());
 	});
 	console.log(request);
