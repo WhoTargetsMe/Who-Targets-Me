3191cbd90cae17d6c25deffc7b969165ad5ba626
Awais Hussain <awais@arachnys.com>
2017-05-03 19:27:48 +0100

    Constituency map with dummy data
    Added some basic download statistics

---
+++ results.js
@@ -0,0 +1,651 @@
+var UK_GENERAL_ELECTION_RESULTS_2010 = {
+  "Aberavon": 60
+, "Aberconwy": 30
+, "Aberdeen North": 60
+, "Aberdeen South": 60
+, "Aberdeenshire West and Kincardine": 0
+, "Airdrie and Shotts": 60
+, "Aldershot": 30
+, "Aldridge-Brownhills": 30
+, "Altrincham and Sale West": 30
+, "Alyn and Deeside": 60
+, "Amber Valley": 30
+, "Angus": 35
+, "Antrim East": 70
+, "Antrim North": 70
+, "Antrim South": 70
+, "Arfon": 25
+, "Argyll and Bute": 0
+, "Arundel and South Downs": 30
+, "Ashfield": 60
+, "Ashford": 30
+, "Ashton under Lyne": 60
+, "Aylesbury": 30
+, "Ayr, Carrick and Cumnock": 60
+, "Ayrshire Central": 60
+, "Ayrshire North and Arran": 60
+, "Banbury": 30
+, "Banff and Buchan": 35
+, "Barking": 60
+, "Barnsley Central": 60
+, "Barnsley East": 60
+, "Barrow and Furness": 60
+, "Basildon and Billericay": 30
+, "Basildon South and Thurrock East": 30
+, "Basingstoke": 30
+, "Bassetlaw": 60
+, "Bath": 0
+, "Batley and Spen": 60
+, "Battersea": 30
+, "Beaconsfield": 30
+, "Beckenham": 30
+, "Bedford": 30
+, "Bedfordshire Mid": 30
+, "Bedfordshire North East": 30
+, "Bedfordshire South West": 30
+, "Belfast East": 80
+, "Belfast North": 70
+, "Belfast South": 55
+, "Belfast West": 65
+, "Bermondsey and Old Southwark": 0
+, "Berwick-upon-Tweed": 0
+, "Berwickshire, Roxburgh and Selkirk": 0
+, "Bethnal Green and Bow": 60
+, "Beverley and Holderness": 30
+, "Bexhill and Battle": 30
+, "Bexleyheath and Crayford": 30
+, "Birkenhead": 60
+, "Birmingham Edgbaston": 60
+, "Birmingham Erdington": 60
+, "Birmingham Hall Green": 60
+, "Birmingham Hodge Hill": 60
+, "Birmingham Ladywood": 60
+, "Birmingham Northfield": 60
+, "Birmingham Perry Barr": 60
+, "Birmingham Selly Oak": 60
+, "Birmingham Yardley": 0
+, "Bishop Auckland": 60
+, "Blackburn": 60
+, "Blackley and Broughton": 60
+, "Blackpool North and Cleveleys": 30
+, "Blackpool South": 60
+, "Blaenau Gwent": 60
+, "Blaydon": 60
+, "Blyth Valley": 60
+, "Bognor Regis and Littlehampton": 30
+, "Bolsover": 60
+, "Bolton North East": 60
+, "Bolton South East": 60
+, "Bolton West": 60
+, "Bootle": 60
+, "Boston and Skegness": 30
+, "Bosworth": 30
+, "Bournemouth East": 30
+, "Bournemouth West": 30
+, "Bracknell": 30
+, "Bradford East": 0
+, "Bradford South": 60
+, "Bradford West": 60
+, "Braintree": 30
+, "Brecon and Radnorshire": 0
+, "Brent Central": 0
+, "Brent North": 60
+, "Brentford and Isleworth": 30
+, "Brentwood and Ongar": 30
+, "Bridgend": 60
+, "Bridgwater and Somerset West": 30
+, "Brigg and Goole": 30
+, "Brighton Kemptown": 30
+, "Brighton Pavilion": 90
+, "Bristol East": 60
+, "Bristol North West": 30
+, "Bristol South": 60
+, "Bristol West": 0
+, "Broadland": 30
+, "Bromley and Chislehurst": 30
+, "Bromsgrove": 30
+, "Broxbourne": 30
+, "Broxtowe": 30
+, "Buckingham": 20
+, "Burnley": 0
+, "Burton": 30
+, "Bury North": 30
+, "Bury South": 60
+, "Bury St Edmunds": 30
+, "Caerphilly": 60
+, "Caithness, Sutherland and Easter Ross": 0
+, "Calder Valley": 30
+, "Camberwell and Peckham": 60
+, "Camborne and Redruth": 30
+, "Cambridge": 0
+, "Cambridgeshire North East": 30
+, "Cambridgeshire North West": 30
+, "Cambridgeshire South East": 30
+, "Cambridgeshire South": 30
+, "Cannock Chase": 30
+, "Canterbury": 30
+, "Cardiff Central": 0
+, "Cardiff North": 30
+, "Cardiff South and Penarth": 60
+, "Cardiff West": 60
+, "Carlisle": 30
+, "Carmarthen East and Dinefwr": 25
+, "Carmarthen West and Pembrokeshire South": 30
+, "Carshalton and Wallington": 0
+, "Castle Point": 30
+, "Ceredigion": 0
+, "Charnwood": 30
+, "Chatham and Aylesford": 30
+, "Cheadle": 0
+, "Chelmsford": 30
+, "Chelsea and Fulham": 30
+, "Cheltenham": 0
+, "Chesham and Amersham": 30
+, "Chester, City of": 30
+, "Chesterfield": 60
+, "Chichester": 30
+, "Chingford and Woodford Green": 30
+, "Chippenham": 0
+, "Chipping Barnet": 30
+, "Chorley": 60
+, "Christchurch": 30
+, "Cities of London and Westminster": 30
+, "Clacton": 30
+, "Cleethorpes": 30
+, "Clwyd South": 60
+, "Clwyd West": 30
+, "Coatbridge, Chryston and Bellshill": 60
+, "Colchester": 0
+, "Colne Valley": 30
+, "Congleton": 30
+, "Copeland": 60
+, "Corby": 30
+, "Cornwall North": 0
+, "Cornwall South East": 30
+, "Cotswolds, The": 30
+, "Coventry North East": 60
+, "Coventry North West": 60
+, "Coventry South": 60
+, "Crawley": 30
+, "Crewe and Nantwich": 30
+, "Croydon Central": 30
+, "Croydon North": 60
+, "Croydon South": 30
+, "Cumbernauld, Kilsyth and Kirkintilloch East": 60
+, "Cynon Valley": 60
+, "Dagenham and Rainham": 60
+, "Darlington": 60
+, "Dartford": 30
+, "Daventry": 30
+, "Delyn": 60
+, "Denton and Reddish": 60
+, "Derby North": 60
+, "Derby South": 60
+, "Derbyshire Dales": 30
+, "Derbyshire Mid": 30
+, "Derbyshire North East": 60
+, "Derbyshire South": 30
+, "Devizes": 30
+, "Devon Central": 30
+, "Devon East": 30
+, "Devon North": 0
+, "Devon South West": 30
+, "Devon West and Torridge": 30
+, "Dewsbury": 30
+, "Don Valley": 60
+, "Doncaster Central": 60
+, "Doncaster North": 60
+, "Dorset Mid and Poole North": 0
+, "Dorset North": 30
+, "Dorset South": 30
+, "Dorset West": 30
+, "Dover": 30
+, "Down North": 20
+, "Down South": 55
+, "Dudley North": 60
+, "Dudley South": 30
+, "Dulwich and West Norwood": 60
+, "Dumfries and Galloway": 60
+, "Dumfriesshire, Clydesdale and Tweeddale": 30
+, "Dunbartonshire East": 0
+, "Dunbartonshire West": 60
+, "Dundee East": 35
+, "Dundee West": 60
+, "Dunfermline and West Fife": 60
+, "Durham North West": 60
+, "Durham North": 60
+, "Durham, City of": 60
+, "Dwyfor Meirionnydd": 25
+, "Ealing Central and Acton": 30
+, "Ealing North": 60
+, "Ealing Southall": 60
+, "Easington": 60
+, "East Ham": 60
+, "East Kilbride, Strathaven and Lesmahagow": 60
+, "East Lothian": 60
+, "Eastbourne": 0
+, "Eastleigh": 0
+, "Eddisbury": 30
+, "Edinburgh East": 60
+, "Edinburgh North and Leith": 60
+, "Edinburgh South West": 60
+, "Edinburgh South": 60
+, "Edinburgh West": 0
+, "Edmonton": 60
+, "Ellesmere Port and Neston": 60
+, "Elmet and Rothwell": 30
+, "Eltham": 60
+, "Enfield North": 30
+, "Enfield Southgate": 30
+, "Epping Forest": 30
+, "Epsom and Ewell": 30
+, "Erewash": 30
+, "Erith and Thamesmead": 60
+, "Esher and Walton": 30
+, "Exeter": 60
+, "Falkirk": 60
+, "Fareham": 30
+, "Faversham and Kent Mid": 30
+, "Feltham and Heston": 60
+, "Fermanagh and South Tyrone": 65
+, "Fife North East": 0
+, "Filton and Bradley Stoke": 30
+, "Finchley and Golders Green": 30
+, "Folkestone and Hythe": 30
+, "Forest of Dean": 30
+, "Foyle": 55
+, "Fylde": 30
+, "Gainsborough": 30
+, "Garston and Halewood": 60
+, "Gateshead": 60
+, "Gedling": 60
+, "Gillingham and Rainham": 30
+, "Glasgow Central": 60
+, "Glasgow East": 60
+, "Glasgow North East": 60
+, "Glasgow North West": 60
+, "Glasgow North": 60
+, "Glasgow South West": 60
+, "Glasgow South": 60
+, "Glenrothes": 60
+, "Gloucester": 30
+, "Gordon": 0
+, "Gosport": 30
+, "Gower": 60
+, "Grantham and Stamford": 30
+, "Gravesham": 30
+, "Great Grimsby": 60
+, "Great Yarmouth": 30
+, "Greenwich and Woolwich": 60
+, "Guildford": 30
+, "Hackney North and Stoke Newington": 60
+, "Hackney South and Shoreditch": 60
+, "Halesowen and Rowley Regis": 30
+, "Halifax": 60
+, "Haltemprice and Howden": 30
+, "Halton": 60
+, "Hammersmith": 60
+, "Hampshire East": 30
+, "Hampshire North East": 30
+, "Hampshire North West": 30
+, "Hampstead and Kilburn": 60
+, "Harborough": 30
+, "Harlow": 30
+, "Harrogate and Knaresborough": 30
+, "Harrow East": 30
+, "Harrow West": 60
+, "Hartlepool": 60
+, "Harwich and Essex North": 30
+, "Hastings and Rye": 30
+, "Havant": 30
+, "Hayes and Harlington": 60
+, "Hazel Grove": 0
+, "Hemel Hempstead": 30
+, "Hemsworth": 60
+, "Hendon": 30
+, "Henley": 30
+, "Hereford and Herefordshire South": 30
+, "Herefordshire North": 30
+, "Hertford and Stortford": 30
+, "Hertfordshire North East": 30
+, "Hertfordshire South West": 30
+, "Hertsmere": 30
+, "Hexham": 30
+, "Heywood and Middleton": 60
+, "High Peak": 30
+, "Hitchin and Harpenden": 30
+, "Holborn and St Pancras": 60
+, "Hornchurch and Upminster": 30
+, "Hornsey and Wood Green": 0
+, "Horsham": 30
+, "Houghton and Sunderland South": 60
+, "Hove": 30
+, "Huddersfield": 60
+, "Hull East": 60
+, "Hull North": 60
+, "Hull West and Hessle": 60
+, "Huntingdon": 30
+, "Hyndburn": 60
+, "Ilford North": 30
+, "Ilford South": 60
+, "Inverclyde": 60
+, "Inverness, Nairn, Badenoch and Strathspey": 0
+, "Ipswich": 30
+, "Isle of Wight": 30
+, "Islington North": 60
+, "Islington South and Finsbury": 60
+, "Islwyn": 60
+, "Jarrow": 60
+, "Keighley": 30
+, "Kenilworth and Southam": 30
+, "Kensington": 30
+, "Kettering": 30
+, "Kilmarnock and Loudoun": 60
+, "Kingston and Surbiton": 0
+, "Kingswood": 30
+, "Kirkcaldy and Cowdenbeath": 60
+, "Knowsley": 60
+, "Lagan Valley": 70
+, "Lanark and Hamilton East": 60
+, "Lancashire West": 60
+, "Lancaster and Fleetwood": 30
+, "Leeds Central": 60
+, "Leeds East": 60
+, "Leeds North East": 60
+, "Leeds North West": 0
+, "Leeds West": 60
+, "Leicester East": 60
+, "Leicester South": 60
+, "Leicester West": 60
+, "Leicestershire North West": 30
+, "Leicestershire South": 30
+, "Leigh": 60
+, "Lewes": 0
+, "Lewisham Deptford": 60
+, "Lewisham East": 60
+, "Lewisham West and Penge": 60
+, "Leyton and Wanstead": 60
+, "Lichfield": 30
+, "Lincoln": 30
+, "Linlithgow and Falkirk East": 60
+, "Liverpool Riverside": 60
+, "Liverpool Walton": 60
+, "Liverpool Wavertree": 60
+, "Liverpool West Derby": 60
+, "Livingston": 60
+, "Llanelli": 60
+, "Londonderry East": 70
+, "Loughborough": 30
+, "Louth and Horncastle": 30
+, "Ludlow": 30
+, "Luton North": 60
+, "Luton South": 60
+, "Macclesfield": 30
+, "Maidenhead": 30
+, "Maidstone and The Weald": 30
+, "Makerfield": 60
+, "Maldon": 30
+, "Manchester Central": 60
+, "Manchester Gorton": 60
+, "Manchester Withington": 0
+, "Mansfield": 60
+, "Meon Valley": 30
+, "Meriden": 30
+, "Merthyr Tydfil and Rhymney": 60
+, "Middlesbrough South and Cleveland East": 60
+, "Middlesbrough": 60
+, "Midlothian": 60
+, "Milton Keynes North": 30
+, "Milton Keynes South": 30
+, "Mitcham and Morden": 60
+, "Mole Valley": 30
+, "Monmouth": 30
+, "Montgomeryshire": 30
+, "Moray": 35
+, "Morecambe and Lunesdale": 30
+, "Morley and Outwood": 60
+, "Motherwell and Wishaw": 60
+, "Na H-Eileanan An Iar": 35
+, "Neath": 60
+, "New Forest East": 30
+, "New Forest West": 30
+, "Newark": 30
+, "Newbury": 30
+, "Newcastle upon Tyne Central": 60
+, "Newcastle upon Tyne East": 60
+, "Newcastle upon Tyne North": 60
+, "Newcastle-under-Lyme": 60
+, "Newport East": 60
+, "Newport West": 60
+, "Newry and Armagh": 65
+, "Newton Abbot": 30
+, "Norfolk Mid": 30
+, "Norfolk North West": 30
+, "Norfolk North": 0
+, "Norfolk South West": 30
+, "Norfolk South": 30
+, "Normanton, Pontefract and Castleford": 60
+, "Northampton North": 30
+, "Northampton South": 30
+, "Northamptonshire South": 30
+, "Norwich North": 30
+, "Norwich South": 0
+, "Nottingham East": 60
+, "Nottingham North": 60
+, "Nottingham South": 60
+, "Nuneaton": 30
+, "Ochil and South Perthshire": 60
+, "Ogmore": 60
+, "Old Bexley and Sidcup": 30
+, "Oldham East and Saddleworth": 60
+, "Oldham West and Royton": 60
+, "Orkney and Shetland": 0
+, "Orpington": 30
+, "Oxford East": 60
+, "Oxford West and Abingdon": 30
+, "Paisley and Renfrewshire North": 60
+, "Paisley and Renfrewshire South": 60
+, "Pendle": 30
+, "Penistone and Stocksbridge": 60
+, "Penrith and The Border": 30
+, "Perth and North Perthshire": 35
+, "Peterborough": 30
+, "Plymouth Moor View": 60
+, "Plymouth Sutton and Devonport": 30
+, "Pontypridd": 60
+, "Poole": 30
+, "Poplar and Limehouse": 60
+, "Portsmouth North": 30
+, "Portsmouth South": 0
+, "Preseli Pembrokeshire": 30
+, "Preston": 60
+, "Pudsey": 30
+, "Putney": 30
+, "Rayleigh and Wickford": 30
+, "Reading East": 30
+, "Reading West": 30
+, "Redcar": 0
+, "Redditch": 30
+, "Reigate": 30
+, "Renfrewshire East": 60
+, "Rhondda": 60
+, "Ribble South": 30
+, "Ribble Valley": 30
+, "Richmond (Yorks)": 30
+, "Richmond Park": 30
+, "Rochdale": 60
+, "Rochester and Strood": 30
+, "Rochford and Southend East": 30
+, "Romford": 30
+, "Romsey and Southampton North": 30
+, "Ross, Skye and Lochaber": 0
+, "Rossendale and Darwen": 30
+, "Rother Valley": 60
+, "Rotherham": 60
+, "Rugby": 30
+, "Ruislip, Northwood and Pinner": 30
+, "Runnymede and Weybridge": 30
+, "Rushcliffe": 30
+, "Rutherglen and Hamilton West": 60
+, "Rutland and Melton": 30
+, "Saffron Walden": 30
+, "Salford and Eccles": 60
+, "Salisbury": 30
+, "Scarborough and Whitby": 30
+, "Scunthorpe": 60
+, "Sedgefield": 60
+, "Sefton Central": 60
+, "Selby and Ainsty": 30
+, "Sevenoaks": 30
+, "Sheffield Brightside and Hillsborough": 60
+, "Sheffield Central": 60
+, "Sheffield Hallam": 0
+, "Sheffield Heeley": 60
+, "Sheffield South East": 60
+, "Sherwood": 30
+, "Shipley": 30
+, "Shrewsbury and Atcham": 30
+, "Shropshire North": 30
+, "Sittingbourne and Sheppey": 30
+, "Skipton and Ripon": 30
+, "Sleaford and North Hykeham": 30
+, "Slough": 60
+, "Solihull": 0
+, "Somerset North East": 30
+, "Somerset North": 30
+, "Somerton and Frome": 0
+, "South Holland and The Deepings": 30
+, "South Shields": 60
+, "Southampton Itchen": 60
+, "Southampton Test": 60
+, "Southend West": 30
+, "Southport": 0
+, "Spelthorne": 30
+, "St Albans": 30
+, "St Austell and Newquay": 0
+, "St Helens North": 60
+, "St Helens South and Whiston": 60
+, "St Ives": 0
+, "Stafford": 30
+, "Staffordshire Moorlands": 30
+, "Staffordshire South": 30
+, "Stalybridge and Hyde": 60
+, "Stevenage": 30
+, "Stirling": 60
+, "Stockport": 60
+, "Stockton North": 60
+, "Stockton South": 30
+, "Stoke-on-Trent Central": 60
+, "Stoke-on-Trent North": 60
+, "Stoke-on-Trent South": 60
+, "Stone": 30
+, "Stourbridge": 30
+, "Strangford": 70
+, "Stratford-on-Avon": 30
+, "Streatham": 60
+, "Stretford and Urmston": 60
+, "Stroud": 30
+, "Suffolk Central and Ipswich North": 30
+, "Suffolk Coastal": 30
+, "Suffolk South": 30
+, "Suffolk West": 30
+, "Sunderland Central": 60
+, "Surrey East": 30
+, "Surrey Heath": 30
+, "Surrey South West": 30
+, "Sussex Mid": 30
+, "Sutton and Cheam": 0
+, "Sutton Coldfield": 30
+, "Swansea East": 60
+, "Swansea West": 60
+, "Swindon North": 30
+, "Swindon South": 30
+, "Tamworth": 30
+, "Tatton": 30
+, "Taunton Deane": 0
+, "Telford": 60
+, "Tewkesbury": 30
+, "Thanet North": 30
+, "Thanet South": 30
+, "Thornbury and Yate": 0
+, "Thurrock": 30
+, "Tiverton and Honiton": 30
+, "Tonbridge and Malling": 30
+, "Tooting": 60
+, "Torbay": 0
+, "Torfaen": 60
+, "Totnes": 30
+, "Tottenham": 60
+, "Truro and Falmouth": 30
+, "Tunbridge Wells": 30
+, "Twickenham": 0
+, "Tynemouth": 60
+, "Tyneside North": 60
+, "Tyrone West": 65
+, "Ulster Mid": 65
+, "Upper Bann": 70
+, "Uxbridge and Ruislip South": 30
+, "Vale of Clwyd": 60
+, "Vale of Glamorgan": 30
+, "Vauxhall": 60
+, "Wakefield": 60
+, "Wallasey": 60
+, "Walsall North": 60
+, "Walsall South": 60
+, "Walthamstow": 60
+, "Wansbeck": 60
+, "Wantage": 30
+, "Warley": 60
+, "Warrington North": 60
+, "Warrington South": 30
+, "Warwick and Leamington": 30
+, "Warwickshire North": 30
+, "Washington and Sunderland West": 60
+, "Watford": 30
+, "Waveney": 30
+, "Wealden": 30
+, "Weaver Vale": 30
+, "Wellingborough": 30
+, "Wells": 0
+, "Welwyn Hatfield": 30
+, "Wentworth and Dearne": 60
+, "West Bromwich East": 60
+, "West Bromwich West": 60
+, "West Ham": 60
+, "Westminster North": 60
+, "Westmorland and Lonsdale": 0
+, "Weston-Super-Mare": 30
+, "Wigan": 60
+, "Wiltshire North": 30
+, "Wiltshire South West": 30
+, "Wimbledon": 30
+, "Winchester": 30
+, "Windsor": 30
+, "Wirral South": 60
+, "Wirral West": 30
+, "Witham": 30
+, "Witney": 30
+, "Woking": 30
+, "Wokingham": 30
+, "Wolverhampton North East": 60
+, "Wolverhampton South East": 60
+, "Wolverhampton South West": 30
+, "Worcester": 30
+, "Worcestershire Mid": 30
+, "Worcestershire West": 30
+, "Workington": 60
+, "Worsley and Eccles South": 60
+, "Worthing East and Shoreham": 30
+, "Worthing West": 30
+, "Wrekin, The": 30
+, "Wrexham": 60
+, "Wycombe": 30
+, "Wyre and Preston North": 30
+, "Wyre Forest": 30
+, "Wythenshawe and Sale East": 60
+, "Yeovil": 0
+, "Ynys Mon": 60
+, "York Central": 60
+, "York Outer": 30
+, "Yorkshire East": 30
+};
