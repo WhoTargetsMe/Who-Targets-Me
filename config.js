532f03a96ac52651ada668594d71e39538bb6b77
Jan Baykara <jan@baykara.co.uk>
2017-05-03 14:43:05 +0100

    Not a bug, per se, but replaced weird comma with hyphen.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = { DEV_ENV: typeof chrome.runtime.getManifest().update_url === 'undefined'}
 
-config.APIURL = config.DEV_ENV ? "https://who-targets-me-staging.herokuapp.com" : "https://who-targets-me.herokuapp.com",
+config.APIURL = config.DEV_ENV ? "https://who-targets-me-staging.herokuapp.com" : "https://who-targets-me.herokuapp.com";
 
 config.devlog = function() {
 	if(config.DEV_ENV) console.log.apply(null, arguments);
