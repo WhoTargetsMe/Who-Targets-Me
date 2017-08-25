70aabd91f08e7be9eea74851d99b26d320418fbb
Alice Rose <alice@acolorbright.com>
2017-08-25 22:42:01 +0200

    Move copyright and external links to footer on PageResults. Translate remaining strings (unproofed).

--- src/frontend/components/PageResults/PageResults.js
+++ src/frontend/components/PageResults/PageResults.js
@@ -49,23 +49,13 @@ export default class PageRegister extends Component {
 
     return (
       <div>
-        <Row style={{height: '100%', paddingTop: '20px', paddingBottom: '20px', margin: 'auto 10px'}}>
+        <Row style={{paddingTop: '20px', paddingBottom: '20px', margin: 'auto 10px'}}>
           <Col sm="1/2" style={{overflow: 'scroll'}}>
             <div className="statbox inverted an-or-2">
               <img src={IMGLogo} style={{height: '150px'}} />
               <div style={{width: '100%'}}>
                 <p>{"Who Targets Me arbeitet im Hintergrund, um zu bestimmen, welche Werbung du siehst. Diese Seite wird mit Statistiken aktualisiert, sobald wir Daten gesammelt haben."}</p>
               </div>
-              <div style={{width: '100%'}}>
-                <Button type="link" href="https://whotargets.me/de/">Webseite</Button>
-                <Button type="link" href="https://whotargets.me/de/terms/">Bedingungen</Button>
-                <Button type="link" href="https://whotargets.me/de/privacy-policy/">Privatsphäre</Button>
-              </div>
-              <div style={{width: '100%'}}>
-                <Button type="link" href="https://www.facebook.com/whotargetsme/" style={{color: '#6d84b4'}}>Facebook</Button>
-                <Button type="link" href="https://twitter.com/whotargetsme" style={{color: '#00aced'}}>Twitter</Button>
-              </div>
-              <p>Copyright 2017 Who Targets Me? Limited</p>
             </div>
           </Col>
           <Col sm="1/2" style={{overflow: 'scroll'}}>
@@ -73,11 +63,19 @@ export default class PageRegister extends Component {
                 <h2>{this.state.userData.constituency.name}</h2>
                 <h4>Mein Bundestagswahlkreis</h4>
                 <hr/>
-                <p>{this.state.userData.constituency.users === 1 ? "Congratulations! You're the first volunteer in your constituency. Can you help us find more?" : "Du bist einer von "}<b>{this.state.userData.constituency.users}</b>{" Freiwilligen in " + this.state.userData.constituency.name + ", kannst du uns helfen "}<b>{roundUp(this.state.userData.constituency.users)}</b>{" zu erreichen?"}</p>
-                <Button type="hollow-success" style={{color: '#3b5998', borderColor: '#3b5998'}} href={shareLinkFB()}>Share on FB</Button> <Button type="hollow-success" style={{color: '#00aced', borderColor: '#00aced'}} href={shareLinkTwitter()} >Share on Twitter</Button>
+                <p>{this.state.userData.constituency.users === 1 ? "Glückwünsch! Du bist der/die erste/r Freiwillige in deiner Bundestagswahlkreis. Kannst du uns helfen, noch mehr zu finden?" : "Du bist einer von "}<b>{this.state.userData.constituency.users}</b>{" Freiwilligen in " + this.state.userData.constituency.name + ", kannst du uns helfen "}<b>{roundUp(this.state.userData.constituency.users)}</b>{" zu erreichen?"}</p>
+                <Button type="hollow-success" style={{color: '#3b5998', borderColor: '#3b5998'}} href={shareLinkFB()}>Auf FB teilen</Button> <Button type="hollow-success" style={{color: '#00aced', borderColor: '#00aced'}} href={shareLinkTwitter()} >Auf Twitter teilen</Button>
                 <p>Teile Who Targets Me mit deinen Freunden, um faire und transparente Kampagnen zu unterstützen.</p>
               </div>
           </Col>
+        </Row>
+        <Row style={{position: 'absolute', 'bottom': '0', textAlign: 'center', fontSize: '12px', paddingTop: '20px', marginLeft: '10px', marginRight: '10px'}}>
+          <p>Copyright 2017 Who Targets Me? Limited</p>
+          <Button type="link" href="https://whotargets.me/de/">Webseite</Button>
+          <Button type="link" href="https://whotargets.me/de/terms/">Bedingungen</Button>
+          <Button type="link" href="https://whotargets.me/de/privacy-policy/">Privatsphäre</Button>
+          <Button type="link" href="https://www.facebook.com/whotargetsme/" style={{color: '#6d84b4'}}>Facebook</Button>
+          <Button type="link" href="https://twitter.com/whotargetsme" style={{color: '#00aced'}}>Twitter</Button>
         </Row>
       </div>
     )
