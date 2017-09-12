1fb20d802fae75f1013fa0b5052e59c4cc861ec4
Jan Baykara <jan@baykara.co.uk>
2017-09-12 05:40:28 -0300

    Linting corrections

--- src/daemon/FacebookAdvertObserver.js
+++ src/daemon/FacebookAdvertObserver.js
@@ -37,68 +37,74 @@ export default new Observer({
     const text = sponsoredText[lang] || sponsoredText.en;
     $(`a:contains(${text})`).each((index, advert) => {
       let container = $(advert).closest('[data-testid="fbfeed_story"]'),
-          fbStoryId = container.attr('id');
+        fbStoryId = container.attr('id'),
+        $chevronButton = container.find('[data-testid="post_chevron_button"]'),
+        chevronID = $chevronButton.attr("id"),
+        id,
+        rationale;
       if (container.length < 1 || temp.saved[fbStoryId]) {
         return;
       }
 
-      console.log("Found ad",fbStoryId);
+      console.log("Found ad", fbStoryId);
 
-  		// Collect 'Why am I seeing this?' rationale
+      // Collect 'Why am I seeing this?' rationale
       // (building on Awais' work in https://github.com/WhoTargetsMe/Who-Targets-Me/commit/2515443542798bf0fafecdfcfad2333380ac71ae)
-      var $chevronButton = container.find('[data-testid="post_chevron_button"]'),
-          chevronID = $chevronButton.attr("id");
-
-			if ($chevronButton[0] && !temp.saved[fbStoryId]) {
-				// Click the chevron button twice to show and then hide the dialog box. It will now appear in the DOM
-				console.log(`Click chevron #${chevronID} twice on ad: ${fbStoryId}`)
-				$chevronButton.get(0).click()
-				$chevronButton.get(0).click()
+      if ($chevronButton[0] && !temp.saved[fbStoryId]) {
+        // Click the chevron button twice to show and then hide the dialog box. It will now appear in the DOM
+        console.log(`Click chevron #${chevronID} twice on ad: ${fbStoryId}`);
+        $chevronButton.get(0).click();
+        $chevronButton.get(0).click();
 
         // Each chevron (popup spawn btn) has an ID referenced by the popup ('data-ownwerid' property).
-        var $rationaleButton = $(`[data-ownerid='${chevronID}']`).find("a[data-feed-option-name='FeedAdSeenReasonOption']")
-        var ajaxify = $rationaleButton.attr('ajaxify')
-        var id_reg_ex = new RegExp("id=\s*(.*?)\s*&")
-        try {
-          var id = id_reg_ex.exec(ajaxify)[1] // get the ad id
-        } catch(e) {
-          // The popup may not have spawned straight away, meaning no Ad ID.
+        let $rationaleButton = $(`[data-ownerid='${chevronID}']`).find("a[data-feed-option-name='FeedAdSeenReasonOption']"),
+          ajaxify = $rationaleButton.attr('ajaxify'),
+          id_reg_ex = new RegExp("id=\s*(.*?)\s*&"),
+          url,
+          xhr;
+
+        try { // get the ad id
+          let id = id_reg_ex.exec(ajaxify)[1];
+        } catch (e) { // The popup may not have spawned straight away, meaning no Ad ID.
           return false;
         }
-        temp.saved[fbStoryId] = { chevronID }
+        temp.saved[fbStoryId] = {chevronID};
 
         // Now fetch the URL.
-        if(temp.saved[fbStoryId].advertID) return false;
-        var url = `https://www.facebook.com/ads/preferences/dialog/?id=${id}&optout_url=http%3A%2F%2Fwww.facebook.com%2Fabout%2Fads&page_type=16&show_ad_choices=0&dpr=1&__a=1`
+        if (temp.saved[fbStoryId].advertID) {
+          return false;
+        }
+
+        url = `https://www.facebook.com/ads/preferences/dialog/?id=${id}&optout_url=http%3A%2F%2Fwww.facebook.com%2Fabout%2Fads&page_type=16&show_ad_choices=0&dpr=1&__a=1`;
         console.log(`${fbStoryId} => ${id}. Making HTTP request to ${url}`);
 
         console.group("Fetch rationale");
-        console.log("Attempting to fetch rationale from",url)
+        console.log("Attempting to fetch rationale from", url);
         // TODO: Consider making URL fetch + payload push asynchronous.
-        var xhr = new XMLHttpRequest();
+        xhr = new XMLHttpRequest();
         xhr.open("GET", url, false);
-        xhr.send()
+        xhr.send();
 
         if (xhr.status === 200) {
-          console.log("HTTP request returned status: %s", xhr.status)
-          var response = JSON.parse(xhr.response.slice(9))
-          var rationale = JSON.stringify(response.jsmods.markup)
-          console.info("Why am I seeing this information?",rationale)
+          console.log("HTTP request returned status: %s", xhr.status);
+          let response = JSON.parse(xhr.response.slice(9)),
+            rationale = JSON.stringify(response.jsmods.markup);
+          console.info("Why am I seeing this information?", rationale);
         } else {
-          console.warn("HTTP response",xhr.status)
+          console.warn("HTTP response", xhr.status);
         }
         console.groupEnd();
 
         temp.saved[fbStoryId].advertID = id;
         temp.saved[fbStoryId].rationale = rationale;
-			}
+      }
 
       payload.push({
         clientTimeObserved: Date.now(),
         html: container.html(),
         rationale,
         fbStoryId,
-        advertID: id,
+        advertID: id
       });
     });
 
