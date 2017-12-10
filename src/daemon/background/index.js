9afde6385920be275e6456c1d2ac2d2988b1fadd
Louis Knight-Webb <stunningpixels@users.noreply.github.com>
2017-12-10 13:55:42 +0000

    Create blank users

--- src/daemon/background/index.js
+++ src/daemon/background/index.js
@@ -1,4 +1,21 @@
 import api from '../api';
+import '../../common/chromeStorage.js';
+
+const createBlankUser = () => { // Create user record and get auth token without user details
+  return new Promise((resolve, reject) => {
+    api.post('user/create', {json: {blank: true}})
+      .then((response) => {
+        if (response.jsonData.errorMessage !== undefined) {
+          throw new Error(response.jsonData.errorMessage);
+        }
+        const {jsonData: {data}} = response;
+        resolve(data.token);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
 
 const initBackground = () => {
 
@@ -8,7 +25,16 @@ const initBackground = () => {
     .then((response) => {
       const {jsonData: {data}} = response;
       if (!data.authvalid) { // Client needs a new auth token
-        //
+        createBlankUser()
+          .then((token) => {
+            chrome.storage.promise.local.set({'general_token': token})
+              .then((res) => {
+                console.log(token, res);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          });
       }
     })
     .catch((err) => {
