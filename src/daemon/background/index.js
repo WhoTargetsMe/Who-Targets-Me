e9d1948f72ac0370e4a440c6f45d79013fbd86d6
Louis Knight-Webb <stunningpixels@users.noreply.github.com>
2018-09-29 11:43:42 +0100

    Initial changes to support YouGov participants

--- src/daemon/background/index.js
+++ src/daemon/background/index.js
@@ -1,6 +1,10 @@
 import api from '../api';
 import '../../common/chromeStorage.js';
 
+// Matches the URLs of participants in YouGov group, US Midterms 2018
+const YOUGOV_REGEX = /https:\/\/g4-us\.yougov\.com\/([a-zA-Z0-9]*)$/
+const YOUGOV_MATCH_PATTERN = 'https://g4-us.yougov.com/*'
+
 const createBlankUser = () => { // Create user record and get auth token without user details
   return new Promise((resolve, reject) => {
     api.post('user/create', {json: {blank: true}})
@@ -17,30 +21,72 @@ const createBlankUser = () => { // Create user record and get auth token without
   });
 };
 
+const createYouGovUser = (participantId) => { // Create user record and get auth token without user details
+  return new Promise((resolve, reject) => {
+    api.post('user/create', {json: {yougov: participantId}})
+      .then((response) => {
+        if (response.jsonData.errorMessage !== undefined) {
+          throw new Error(response.jsonData.errorMessage);
+        }
+        const {jsonData: {data}} = response;
+        resolve(data.token);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+const checkYouGovUrl = url => {
+  // Check if the URL is YouGov
+  const rawYouGovRegexExec = YOUGOV_REGEX.exec(url)
+  if (rawYouGovRegexExec) {
+    // Remove listener and attempt to create a new user
+    createYouGovUser(rawYouGovRegexExec[1])
+    return true
+  }
+  return false
+}
+
+// Add Chrome Tabs listener for all URLs
+const listener = (tabId, {status}, {url}) => {
+  if (status === 'complete') {
+    if (checkYouGovUrl(url)) {
+      chrome.tabs.onUpdated.removeListener(listener)
+    }
+  }
+};
+
+const initYouGovListener = () => {
+  // Check currently open tabs before starting event listener
+  let urlMatchedCurrentlyOpenTab = false
+
+  // Check current tabs for YouGov
+  chrome.tabs.query({url: YOUGOV_MATCH_PATTERN}, tabs => {
+    // Loop through tabs until match is found
+    for (let tab of tabs) {
+      if (checkYouGovUrl(tab.url)) {
+        urlMatchedCurrentlyOpenTab = true;
+        break;
+      }
+    }
+  });
+
+  // No match was found, init the listener in-case future tabs match
+  if (!urlMatchedCurrentlyOpenTab) {
+    chrome.tabs.onUpdated.addListener(listener)
+  }
+}
+
 const initBackground = () => {
+  api.get('general/authvalid')
+    .then(response => {
+      const {jsonData: {data}} = response;
 
-  /* 'PHONE HOME' to server and determine if client needs to generate an auth token or not */
-
-  // api.get('general/authvalid')
-  //   .then((response) => {
-  //     const {jsonData: {data}} = response;
-  //     if (!data.authvalid) { // Client needs a new auth token
-  //       createBlankUser()
-  //         .then((token) => {
-  //           chrome.storage.promise.local.set({'general_token': token})
-  //             .then((res) => {
-  //               console.log(token, res);
-  //             })
-  //             .catch((error) => {
-  //               console.log(error);
-  //             });
-  //         });
-  //     }
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
-
+      if (!data.authvalid) {
+        initYouGovListener();
+      }
+    })
 };
 
 export default initBackground;
