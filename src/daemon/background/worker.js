8b42871eae9b92dee8b44f222b28161dab4d9697
danazsdiq <danazsdiq17@gmail.com>
2022-12-12 11:10:50 +0300

    fix: Converting value of isNotifiedRegister from yes to true, ensuring backward-compatibility

--- src/daemon/background/worker.js
+++ src/daemon/background/worker.js
@@ -9,11 +9,19 @@ import {
 chrome.runtime.onInstalled.addListener(async () => {
   const userData = await readStorage("userData");
 
+  /*
+    Changing userData.isNotifiedRegister from "yes" to true will cause issues
+    We handle this change for all extension users to ensure compatibility
+  */
+  if (userData?.isNotifiedRegister === "yes") {
+    await setToStorage("userData", { ...userData, isNotifiedRegister: true });
+  }
+
   if (shouldOpenResultsPage(userData)) {
     chrome.tabs.create({
       url: process.env.RESULTS_URL,
     });
-    await setToStorage("userData", { isNotifiedRegister: true });
+    await setToStorage("userData", { ...userData, isNotifiedRegister: true });
   }
 });
 
