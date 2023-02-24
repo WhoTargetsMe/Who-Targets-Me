5e78fef9b7d050095afcad14730b889ea2c32ce7
danazsdiq <danazsdiq17@gmail.com>
2023-02-24 10:40:10 +0300

    fix: Forgot the slashes

--- test-helpers.js
+++ test-helpers.js
@@ -1,15 +1,15 @@
 const path = require("path");
 
 const chromeBuildPath = () => {
-  return path.join(__dirname, "/extensions/chrome");
+  return path.join(__dirname, "/extensions/chrome/");
 };
 
 const firefoxBuildPath = () => {
-  return path.join(__dirname, "/extensions/firefox");
+  return path.join(__dirname, "/extensions/firefox/");
 };
 
 const edgeBuildPath = () => {
-  return path.join(__dirname, "/extensions/edge");
+  return path.join(__dirname, "/extensions/edge/");
 };
 
 exports.chromeBuildPath = chromeBuildPath;
