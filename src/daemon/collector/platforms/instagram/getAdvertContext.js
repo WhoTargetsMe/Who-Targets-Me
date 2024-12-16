b7a5e40da8c075b06cc6bb6478243bdffb43af8e
danazsdiq <danazsdiq17@gmail.com>
2024-12-16 14:37:58 +0300

    fix: Removing query params from Instagram, Twitter, and Facebook

--- src/daemon/collector/platforms/instagram/getAdvertContext.js
+++ src/daemon/collector/platforms/instagram/getAdvertContext.js
@@ -1,7 +1,9 @@
+import { removeQueryParamsFromUrl } from "../../utils";
+
 export const getAdvertContext = () => {
   const url = window.location.href;
 
   if (!url && url.length === 0) return {};
 
-  return { url };
+  return { url: removeQueryParamsFromUrl(url) };
 };
---
+++ src/daemon/collector/utils/removeQueryParamsFromUrl.js
@@ -0,0 +1,3 @@
+export const removeQueryParamsFromUrl = (url) => {
+  return url.split("?")[0];
+};
