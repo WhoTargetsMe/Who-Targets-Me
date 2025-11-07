c09e0bea275469773dea094c46a897bede685dd1
danazsdiq <danazsdiq17@gmail.com>
2025-11-07 17:02:57 +0300

    fix(WTM-1280): conditionally reading general_token

--- src/shared/handlers/handleUserCountry.js
+++ src/shared/handlers/handleUserCountry.js
@@ -4,7 +4,7 @@ import { readStorage } from "../utils/readStorage";
 export const handleUserCountry = async () => {
   const general_token = await readStorage("general_token");
 
-  if (general_token.length === 0) return "";
+  if (!general_token) return "";
 
   const user = await app.service("user-credentials").get(general_token);
 
